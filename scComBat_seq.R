6922eb5aa4ecdf44c7e3544acc2e16e30a9cdc7a
zhangyuqing <zhangyuqing.pkusms@gmail.com>
2018-10-01 14:38:21 -0400

    checked DE simulation pipeline

---
+++ scComBat_seq.R
@@ -0,0 +1,178 @@
+#' Adjust for batch effects using an empirical Bayes framework in RNA-seq raw counts
+#' 
+#' ComBat_seq is an extension to the ComBat method using Negative Binomial model.
+#' 
+#' @param counts Raw count matrix from genomic studies (dimensions gene x sample) 
+#' @param batch Batch covariate (only one batch allowed)
+#' @param mod Model matrix for outcome of interest and other covariates besides batch
+#' 
+#' @return data A probe x sample count matrix, adjusted for batch effects.
+#' 
+#' @examples 
+#' 
+#' rm(list=ls()); load("simdata.RData"); full_mod=TRUE; normalize="none"; source("helper_seq.R")
+#' 
+#' @export
+#' 
+
+ComBat_seq <- function(counts, batch, group, full_mod=TRUE, normalize="none", ref_batch=NULL){
+  ########  Preparation  ########  
+  library(edgeR)  # require bioconductor >= 3.7, edgeR >= 3.22.1, otherwise run  # source("glmfit.R")
+  dge_obj <- DGEList(counts=counts, group=group)
+  
+  ## Prepare characteristics on batches
+  batch <- as.factor(batch)
+  n_batch <- nlevels(batch)  # number of batches
+  batches_ind <- lapply(1:n_batch, function(i){which(batch==levels(batch)[i])}) # list of samples in each batch  
+  n_batches <- sapply(batches_ind, length)
+  #if(any(n_batches==1)){mean_only=TRUE; cat("Note: one batch has only one sample, setting mean.only=TRUE\n")}
+  n_sample <- sum(n_batches)
+  cat("Found",n_batch,'batches\n')
+  
+  ## Select reference
+  if(is.null(ref_batch)){ref_batch <- sample(levels(batch), 1)}  #if not specified, randomly select a batch
+  cat("Using", ref_batch, 'as reference batch\n')
+  
+  ## Make design matrix 
+  # batch
+  batchmod <- model.matrix(~-1+batch)
+  batchmod[, paste0('batch',ref_batch)] <- 1
+  # covariate
+  group <- as.factor(group)
+  if(full_mod & nlevels(group)>1){
+    mod <- model.matrix(~group)
+  }else{
+    mod <- model.matrix(~1, data=as.data.frame(t(counts)))
+  }
+  # combine
+  design <- cbind(batchmod, mod)
+  
+  ## Check for intercept in covariates, and drop if present
+  check <- apply(design, 2, function(x) all(x == 1))
+  #if(!is.null(ref)){check[ref]=FALSE} ## except don't throw away the reference batch indicator
+  check[1:n_batch] <- FALSE
+  design <- as.matrix(design[,!check])
+  cat("Adjusting for",ncol(design)-ncol(batchmod),'covariate(s) or covariate level(s)\n')
+  
+  ## Check if the design is confounded
+  if(qr(design)$rank<ncol(design)){
+    #if(ncol(design)<=(n_batch)){stop("Batch variables are redundant! Remove one or more of the batch variables so they are no longer confounded")}
+    if(ncol(design)==(n_batch+1)){stop("The covariate is confounded with batch! Remove the covariate and rerun ComBat")}
+    if(ncol(design)>(n_batch+1)){
+      if((qr(design[,-c(1:n_batch)])$rank<ncol(design[,-c(1:n_batch)]))){stop('The covariates are confounded! Please remove one or more of the covariates so the design is not confounded')
+      }else{stop("At least one covariate is confounded with batch! Please remove confounded covariates and rerun ComBat")}}
+  }
+
+  ## Check for missing values in count matrix
+  NAs = any(is.na(counts))
+  if(NAs){cat(c('Found',sum(is.na(counts)),'Missing Data Values\n'),sep=' ')}
+
+  
+  ########  Estimate gene-wise dispersions within each batch  ########
+  ## Estimate common dispersion within each batch as an initial value
+  disp_common <- sapply(1:n_batch, function(i){
+    if(n_batches[i]==1){
+      stop("Not supporting 1 sample per batch yet!")
+    }else if(n_batches[i] <= ncol(design)-ncol(batchmod)+1){ 
+      # not enough residual degree of freedom
+      return(estimateGLMCommonDisp(counts[, batches_ind[[i]]], design=NULL, subset=nrow(counts)))
+      #as.matrix(design[batches_ind[[i]], (n_batch+1):ncol(design)]),
+    }else{
+      return(estimateGLMCommonDisp(counts[, batches_ind[[i]]], design=mod[batches_ind[[i]], ], subset=nrow(counts)))
+    }
+  })
+  
+  ## Estimate gene-wise dispersion within each batch 
+  genewise_disp_lst <- lapply(1:n_batch, function(j){
+    if(n_batches[j]==1){
+      stop("Not supporting 1 sample per batch yet!")
+    }else if(n_batches[j] <= ncol(design)-ncol(batchmod)+1){
+      # not enough residual degrees of freedom
+      # return(estimateGLMTagwiseDisp(counts[, batches_ind[[j]]], design=NULL, 
+      #                               dispersion=disp_common[j], prior.df=0))
+      return(rep(disp_common[j], nrow(counts)))
+      #as.matrix(design[batches_ind[[j]], (n_batch+1):ncol(design)]),
+    }else{
+      return(estimateGLMTagwiseDisp(counts[, batches_ind[[j]]], design=mod[batches_ind[[j]], ], 
+                                    dispersion=disp_common[j], prior.df=0))
+    }
+  })
+  names(genewise_disp_lst) <- paste0('batch', levels(batch))
+  
+  ## construct dispersion matrix
+  phi_matrix <- matrix(NA, nrow=nrow(counts), ncol=ncol(counts))
+  for(k in 1:n_batch){
+    phi_matrix[, batches_ind[[k]]] <- vec2mat(genewise_disp_lst[[k]], n_batches[k]) #matrix(rep(genewise_disp_lst[[k]], n_batches[k]), ncol=n_batches[k])
+  }#round(apply(phi_matrix,2,mean),2)
+  
+    
+  ########  Estimate parameters from NB GLM  ########
+  glm_f <- glmFit(dge_obj, design=design, dispersion=phi_matrix, prior.count=0) #no intercept - nonEstimable; compute offset (library sizes) within function
+  # alpha_g <- glm_f$coefficients[, 1:n_batch] %*% as.matrix(n_batches/n_sample) #compute intercept as batch-size-weighted average from batches
+  # new_offset <- t(vec2mat(getOffset(dge_obj), nrow(counts))) +   # original offset - sample (library) size
+  #   vec2mat(alpha_g, ncol(counts))  # new offset - gene background expression
+  # # getOffset(dge_obj) is the same as log(dge_obj$samples$lib.size)
+  # glm_f2 <- glmFit.default(dge_obj$counts, design=design, dispersion=phi_matrix, 
+  #                          offset=new_offset, prior.count=0) 
+  
+  #beta_hat <- glm_f$coefficients[, (n_batch+1):ncol(design)]  #glm_f2$coefficients[, (n_batch+1):ncol(design)]
+  gamma_hat <- glm_f$coefficients[, 2:n_batch] #glm_f2$coefficients[, 1:n_batch]
+  mu_hat <- glm_f$fitted.values #glm_f2$fitted.values
+  phi_hat <- do.call(cbind, genewise_disp_lst)
+  #if(!identical(colnames(gamma_hat), colnames(phi_hat))){stop("gamma and phi don't match!")}
+  #tmp = mu_hat - exp(glm_f2$coefficients %*% t(design) + new_offset); tmp[1:6,1:6]; mean(tmp)
+  
+  
+  ########  In each batch, compute posterior estimation through Monte-Carlo integration  ########  
+  monte_carlo_res <- lapply(1:n_batch, function(ii){
+    monte_carlo_int_NB(dat=counts[, batches_ind[[ii]]], mu=mu_hat[, batches_ind[[ii]]], 
+                       gamma=gamma_hat[, ii], phi=phi_hat[, ii])
+    #dat=counts[, batches_ind[[ii]]]; mu=mu_hat[, batches_ind[[ii]]]; gamma=gamma_hat[, ii]; phi=phi_hat[, ii]
+  })
+  names(monte_carlo_res) <- paste0('batch', levels(batch))
+  
+  gamma_star_mat <- lapply(monte_carlo_res, function(res){res$gamma_star})
+  gamma_star_mat <- do.call(cbind, gamma_star_mat)
+  phi_star_mat <- lapply(monte_carlo_res, function(res){res$phi_star})
+  phi_star_mat <- do.call(cbind, phi_star_mat)
+  
+  
+  ########  Obtain adjusted batch-free distribution  ########
+  mu_star <- matrix(NA, nrow=nrow(counts), ncol=ncol(counts))
+  for(jj in 1:n_batch){
+    mu_star[, batches_ind[[jj]]] <- exp(log(mu_hat[, batches_ind[[jj]]])-
+                                          vec2mat(gamma_star_mat[, jj], n_batches[jj])#-
+                                          #t(vec2mat(getOffset(dge_obj)[batches_ind[[jj]]], nrow(counts)))
+    )
+  }
+  phi_star <- rowMeans(phi_star_mat)
+  
+  
+  ########  Adjust the data  ########  
+  adjust_counts <- matrix(NA, nrow=nrow(counts), ncol=ncol(counts))
+  for(kk in 1:n_batch){
+    counts_sub <- counts[, batches_ind[[kk]]]
+    old_mu <- mu_hat[, batches_ind[[kk]]]
+    old_phi <- phi_hat[, kk]
+    new_mu <- mu_star[, batches_ind[[kk]]]
+    new_phi <- phi_star
+    adjust_counts[, batches_ind[[kk]]] <- match_quantiles(counts_sub=counts_sub, 
+                                                          old_mu=old_mu, old_phi=old_phi, 
+                                                          new_mu=new_mu, new_phi=new_phi)
+  }
+  
+  ## normalize for library size?
+  # if(normalize!="none"){
+  #   if(!(normalize %in% c("TMM","RLE","upperquartile"))){
+  #     stop("Normalization method not supported")
+  #   }else{
+  #     adj_dge_obj <- DGEList(counts=adjust_counts, group=group, lib.size=dge_obj$samples$lib.size)
+  #     adj_dge_obj <- calcNormFactors(adj_dge_obj, method=normalize)
+  #     for(ii in 1:ncol(adjust_counts)){
+  #       adjust_counts[, ii] <- round(adjust_counts[, ii] * adj_dge_obj$samples$norm.factors[ii], 0)
+  #     }
+  #   }
+  # }
+  
+  return(adjust_counts)
+}
--- simulations/sim_DE_pipe_polyester.R
+++ simulations/sim_DE_pipe_polyester.R
@@ -10,19 +10,25 @@ set.seed(123)
 
 
 ####  Parameters
-iterations <- 3 #100  #number of simulations to run
-N_samples <- c(3, 3, 3, 3)  #we want small sample size
-alpha <- 0.05
-#N_genes <- 2000  #number of genes   
-coverage <- 20  
-read_length <- 100
-
 command_args <- commandArgs(trailingOnly=TRUE)
-bio_fold <- as.numeric(command_args[1])  #2  #fold change for biological condition
-batch_fold <- as.numeric(command_args[2])  #3  #fold change for batch effect - assuming that batch behaves as fold change on counts
-exp_name <- paste0("bio", command_args[1], "_batch", command_args[2])
+factor_exam <- command_args[1]  # "FC", "Disp", "Design", "Nsample", "BaseCts"  #the factor in examination in this sim
+bio_fold <- as.numeric(command_args[2])  #2  #fold change for biological condition
+batch_fold <- as.numeric(command_args[3])  #3  #fold change for batch effect - assuming that batch behaves as fold change on counts
+size_1 <- as.numeric(command_args[4])   # 1/dispersion in batch 1 ("Bernard")
+size_2 <- as.numeric(command_args[5])   # 1/dispersion in batch 2 ("Arnold")
+N_total_sample <- as.numeric(command_args[6])  #20  #total number of samples in the study
+balanced <- as.logical(command_args[7])  #TRUE  #logical, if TRUE balanced design
+coverage <- as.numeric(command_args[8])  #20  
+#factor_exam="FC"; bio_fold=1.5; batch_fold=2; size_1=10; size_2=1000; N_total_sample=20; balanced=FALSE; coverage=1
   
+iterations <- 100  #number of simulations to run
+alpha <- 0.05
+exp_name <- paste0("sim", factor_exam, "_bio", bio_fold, "_batch", batch_fold, "_sizes", size_1, '_', size_2,
+                   "_N", N_total_sample, ifelse(balanced, "_B", "_U"), "_depth", coverage)
+exp_name <- gsub('.', '', exp_name, fixed=TRUE)
+
 # FASTA annotation
+read_length <- 100
 fasta_file <- system.file('extdata', 'chr22.fa', package='polyester')
 fasta <- readDNAStringSet(fasta_file)
 # subset the FASTA file to first N_genes transcripts
@@ -30,14 +36,30 @@ fasta <- readDNAStringSet(fasta_file)
 # reads per transcript = transcriptlength/readlength * coverage
 readspertx <- round(coverage * width(fasta) / read_length)
 
-#fold change matrix indicating study design
-fold_changes <- matrix(NA, nrow=length(fasta), ncol=length(N_samples))  
-fold_changes[1:50, ] <- matrix(rep(c(1,bio_fold,batch_fold,bio_fold*batch_fold),50), ncol=length(N_samples), byrow=TRUE)
-fold_changes[51:450, ] <- matrix(rep(c(1,1,batch_fold,batch_fold),400), ncol=length(N_samples), byrow=TRUE)
-fold_changes[451:500, ] <- matrix(rep(c(batch_fold,bio_fold*batch_fold,1,bio_fold),50), ncol=length(N_samples), byrow=TRUE)
-fold_changes[501:length(fasta), ] <- matrix(rep(c(batch_fold,batch_fold,1,1),length(fasta)-500), ncol=length(N_samples), byrow=TRUE)
+# study design
+if(balanced){
+  N_samples <- rep(N_total_sample/4, 4)
+}else{
+  N_samples <- N_total_sample * c(0.1, 0.4, 0.4, 0.1)  #  20%-80% unbalanced
+}
 
-de_ground_truth_ind <- c(rep(1:50), rep(451:500))  # true differentially expressed genes
+fold_changes <- matrix(NA, nrow=length(fasta), ncol=length(N_samples))  
+# fold_changes[1:50, ] <- matrix(rep(c(1,bio_fold,batch_fold,bio_fold*batch_fold),50), ncol=length(N_samples), byrow=TRUE)
+# fold_changes[51:450, ] <- matrix(rep(c(1,1,batch_fold,batch_fold),400), ncol=length(N_samples), byrow=TRUE)
+# fold_changes[451:500, ] <- matrix(rep(c(batch_fold,bio_fold*batch_fold,1,bio_fold),50), ncol=length(N_samples), byrow=TRUE)
+# fold_changes[501:length(fasta), ] <- matrix(rep(c(batch_fold,batch_fold,1,1),length(fasta)-500), ncol=length(N_samples), byrow=TRUE)
+fold_changes[1:50, ] <- matrix(rep(c(1, bio_fold, batch_fold, bio_fold*batch_fold), 50),
+                                ncol=length(N_samples), byrow=TRUE)  # up-regulated
+fold_changes[51:100, ] <- matrix(rep(c(bio_fold, 1, bio_fold*batch_fold, batch_fold), 50),
+                               ncol=length(N_samples), byrow=TRUE)  # down-regulated
+fold_changes[101:length(fasta), ] <- matrix(rep(c(1, 1, batch_fold, batch_fold), length(fasta)-100),
+                                            ncol=length(N_samples), byrow=TRUE)   # "null" genes
+
+size_mat <- matrix(rep(c(size_1, size_1, size_2, size_2), length(fasta)),
+                   ncol=length(N_samples), byrow=TRUE)
+
+#de_ground_truth_ind <- c(1:50, 451:500)  # true differentially expressed genes
+de_ground_truth_ind <- 1:100
 N_DE <- length(de_ground_truth_ind)
 N_nonDE <- length(fasta) - N_DE
 
@@ -47,16 +69,16 @@ for(iter in 1:iterations){
   cat(paste("Simulation", iter, '\n'))
   
   ####  Simulate datasets
-  if(dir.exists('simulated_reads')){unlink('simulated_reads', recursive=TRUE)}
-  simulate_experiment(fasta_file, reads_per_transcript=readspertx, 
-                      num_reps=N_samples, fold_changes=fold_changes, outdir='simulated_reads') 
+  if(dir.exists(exp_name)){unlink(exp_name, recursive=TRUE)}
+  simulate_experiment(fasta_file, reads_per_transcript=readspertx, size=size_mat, 
+                      num_reps=N_samples, fold_changes=fold_changes, outdir=exp_name) 
   
   # load count matrix and remove fastq files to save space
-  load("simulated_reads/sim_counts_matrix.rda")
+  load(file.path(exp_name, "sim_counts_matrix.rda"))
   de_ground_truth <- rownames(counts_matrix)[de_ground_truth_ind]
   
   # batch and biological vectors
-  batch <- c(rep("Bernard", sum(N_samples[1:2])), rep("Arnold", sum(N_samples[3:4])))
+  batch <- c(rep(1, sum(N_samples[1:2])), rep(2, sum(N_samples[3:4])))
   group <- c(rep(0, N_samples[1]), rep(1, N_samples[2]), rep(0, N_samples[3]), rep(1, N_samples[4]))
   
   
@@ -69,7 +91,7 @@ for(iter in 1:iterations){
   fit1 <- glmQLFit(y1, design)
   qlf1 <- glmQLFTest(fit1, coef=2)
   de_res1 <- topTags(qlf1, n=nrow(counts_matrix))$table
-  de_called1 <- rownames(de_res1)[de_res1$FDR < alpha]
+  de_called1 <- rownames(de_res1)[de_res1$PValue < alpha]
   
   tpr1 <- length(intersect(de_called1, de_ground_truth)) / N_DE
   fpr1 <- length(setdiff(de_called1, de_ground_truth)) / N_nonDE
@@ -80,10 +102,10 @@ for(iter in 1:iterations){
   y2 <- calcNormFactors(y2, method="TMM")
   design2 <- model.matrix(~ as.factor(group) + as.factor(batch))
   y2 <- estimateDisp(y2, design2)
-  fit2 <- glmQLFit(y2, design)
+  fit2 <- glmQLFit(y2, design2)
   qlf2 <- glmQLFTest(fit2, coef=2)
   de_res2 <- topTags(qlf2, n=nrow(counts_matrix))$table
-  de_called2 <- rownames(de_res2)[de_res2$FDR < alpha]
+  de_called2 <- rownames(de_res2)[de_res2$PValue < alpha]
   
   tpr2 <- length(intersect(de_called2, de_ground_truth)) / N_DE
   fpr2 <- length(setdiff(de_called2, de_ground_truth)) / N_nonDE
@@ -94,12 +116,12 @@ for(iter in 1:iterations){
   
   y3 <- DGEList(counts=adj_counts_combatseq, group=as.factor(group))
   y3 <- calcNormFactors(y3, method="TMM")
-  design <- model.matrix(~as.factor(group))
-  y3 <- estimateDisp(y3, design)
-  fit3 <- glmQLFit(y3, design)
+  design3 <- model.matrix(~as.factor(group))
+  y3 <- estimateDisp(y3, design3)
+  fit3 <- glmQLFit(y3, design3)
   qlf3 <- glmQLFTest(fit3, coef=2)
   de_res3 <- topTags(qlf3, n=nrow(counts_matrix))$table
-  de_called3 <- rownames(de_res3)[de_res3$FDR < alpha]
+  de_called3 <- rownames(de_res3)[de_res3$PValue < alpha]
   
   tpr3 <- length(intersect(de_called3, de_ground_truth)) / N_DE
   fpr3 <- length(setdiff(de_called3, de_ground_truth)) / N_nonDE
@@ -118,5 +140,8 @@ for(iter in 1:iterations){
   # power (true positive rate)
   write.table(DE_res["tpr", ], sprintf('tpr_%s.csv', exp_name), 
               append=!first.file, col.names=first.file, row.names=FALSE, sep=",")
+  
+  rm(counts_matrix)
 }
 
+if(dir.exists(exp_name)){unlink(exp_name, recursive=TRUE)}
