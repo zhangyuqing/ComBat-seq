00311278dec17b959ec2963c5ac9b8e5cf207a24
zhangyuqing <zhangyuqing.pkusms@gmail.com>
2018-10-28 15:48:59 -0700

    complete TB data report

--- explore_real_data/RNAseq/TB_run_nonzero_disps.R
+++ explore_real_data/RNAseq/TB_run_nonzero_disps.R
@@ -6,7 +6,7 @@ sapply(c("SummarizedExperiment", "DESeq2", "edgeR", "dendextend", "ggplot2", "re
          "ggdendro", "MASS"), require, character.only=TRUE)
 
 ####  Load data
-rds_obj <- readRDS(file.path(output_dir, "combined.rds"))
+rds_obj <- readRDS("combined.rds")
 cts <- assays(rds_obj)$counts
 batch <- colData(rds_obj)$SequencingBatch
 group <- colData(rds_obj)$Label
@@ -14,35 +14,58 @@ glm_ctrl <- glm.control(maxit=200)
 
 
 ####  Define function to compute dispersion
+# calculate dispersion for one gene
+genDisp_1gene <- function(curr_cts, curr_group, glm_ctrl){
+  if(length(unique(curr_cts)) > 2 & var(curr_cts) > 0){
+    if(length(unique(curr_group))<2){
+      curr_f <- try(glm.nb(curr_cts ~ 1, control=glm_ctrl), silent=TRUE)
+    }else{
+      curr_f <- try(glm.nb(curr_cts ~ as.factor(curr_group), control=glm_ctrl), silent=TRUE)
+    }
+    if(class(curr_f)[1]!="try-error"){
+      return(1/summary(curr_f)$theta)
+    }
+  }
+  return(NA)
+}
+
+# use counts containing zeros
+genDisp <- function(cts, group, batch, glm_ctrl, batch_name){
+  disp_full_batch <- sapply(1:nrow(cts), function(ind){
+    genDisp_1gene(curr_cts=cts[ind, batch==batch_name], curr_group=group[batch==batch_name], glm_ctrl=glm_ctrl)
+  })
+  disp_full_batch <- disp_full_batch[!is.na(disp_full_batch)]
+  return(disp_full_batch)
+}
+
+# use counts without zeros
+genDisp_Sep <- function(nonzero_cts, batch_sep, group_sep, glm_ctrl, batch_name){
+  # take the batch specified by batch_name
+  group_sep_thisbatch <- nonzero_cts_thisbatch <- list()
+  for(i in 1:length(nonzero_cts)){
+    group_sep_thisbatch[[i]] <- group_sep[[i]][batch_sep[[i]]==batch_name]
+    nonzero_cts_thisbatch[[i]] <- nonzero_cts[[i]][batch_sep[[i]]==batch_name]
+  }
+  names(nonzero_cts_thisbatch) <- names(group_sep_thisbatch) <- names(nonzero_cts)
+  
+  # calculate dispersions
+  disp_nonzero_thisbatch <- sapply(1:length(nonzero_cts_thisbatch), function(ii){
+    curr_cts <- nonzero_cts_thisbatch[[ii]] 
+    curr_group <- group_sep_thisbatch[[ii]]
+    return(genDisp_1gene(curr_cts, curr_group, glm_ctrl))
+  })
+  disp_nonzero_thisbatch <- disp_nonzero_thisbatch[!is.na(disp_nonzero_thisbatch)]
+  return(disp_nonzero_thisbatch)
+}
+
 
 
 ####  Use counts WITH zeros to estimate dispersion
-# Africa
-disp_full_batch1 <- sapply(1:nrow(cts), function(kk){
-  curr_cts <- cts[kk, batch==1] 
-  if(length(curr_cts) > 2 & var(curr_cts) > 0){
-    curr_f <- try(glm.nb(curr_cts ~ as.factor(group_num[batch==1]), control=glm_ctrl), silent=TRUE)
-    if(class(curr_f)[1]!="try-error"){
-      return(1/summary(curr_f)$theta)
-    }
-  }
-  return(NA)
-})
-disp_full_batch1 <- disp_full_batch1[!is.na(disp_full_batch1)]
-
-# G6
-disp_full_batch2 <- sapply(1:nrow(cts), function(ll){
-  curr_cts <- cts[ll, batch==2] 
-  if(length(curr_cts) > 2 & var(curr_cts) > 0){
-    curr_f <- try(glm.nb(curr_cts ~ as.factor(group_num[batch==2]), control=glm_ctrl), silent=TRUE)
-    if(class(curr_f)[1]!="try-error"){
-      return(1/summary(curr_f)$theta)
-    }
-  }
-  return(NA)
-})
-disp_full_batch2 <- disp_full_batch2[!is.na(disp_full_batch2)]
-
+disp_full_africa <- genDisp(cts=cts, group=group, batch=batch, glm_ctrl=glm_ctrl, batch_name="Africa")
+disp_full_G6 <- genDisp(cts=cts, group=group, batch=batch, glm_ctrl=glm_ctrl, batch_name="G6")
+disp_full_india <- genDisp(cts=cts, group=group, batch=batch, glm_ctrl=glm_ctrl, batch_name="India")
+disp_full_bzl1 <- genDisp(cts=cts, group=group, batch=batch, glm_ctrl=glm_ctrl, batch_name="Brazil_1")
+disp_full_bzl2 <- genDisp(cts=cts, group=group, batch=batch, glm_ctrl=glm_ctrl, batch_name="Brazil_2")
 
 
 
@@ -51,72 +74,28 @@ disp_full_batch2 <- disp_full_batch2[!is.na(disp_full_batch2)]
 batch_sep <- group_sep <- nonzero_cts <- list()
 for(i in 1:nrow(cts)){
   nonzero_ind <- which(cts[i, ]!=0)
-  batch_sep[[i]] <- batch[nonzero_ind]
-  group_sep[[i]] <- group_num[nonzero_ind]
+  batch_sep[[i]] <- as.character(batch[nonzero_ind])
+  group_sep[[i]] <- as.character(group[nonzero_ind])
   nonzero_cts[[i]] <- cts[i, nonzero_ind]
 }
 # sanity check
 if(!identical(sapply(nonzero_cts, length), as.integer(rowSums(cts!=0)))){stop("Error in taking non-zero portions!")}
 names(nonzero_cts) <- rownames(cts); names(batch_sep) <- rownames(cts); names(group_sep) <- rownames(cts)
 
-##  Dispersion in batch 1
-# take batch 1 data
-group_sep_batch1 <- nonzero_cts_batch1 <- list()
-for(i in 1:length(nonzero_cts)){
-  group_sep_batch1[[i]] <- group_sep[[i]][batch_sep[[i]]==1]
-  nonzero_cts_batch1[[i]] <- nonzero_cts[[i]][batch_sep[[i]]==1]
-}
-names(nonzero_cts_batch1) <- names(group_sep_batch1) <- rownames(cts)
-# calculate dispersions
-disp_nonzero_batch1 <- sapply(1:length(nonzero_cts_batch1), function(ii){
-  curr_cts <- nonzero_cts_batch1[[ii]] 
-  curr_group <- group_sep_batch1[[ii]]
-  if(length(curr_cts) > 2 & var(curr_cts) > 0){
-    curr_f <- try(glm.nb(curr_cts ~ as.factor(curr_group), control=glm_ctrl), silent=TRUE)
-    if(class(curr_f)[1]!="try-error"){
-      return(1/summary(curr_f)$theta)
-    }
-  }
-  # curr_y <- DGEList(counts=t(as.matrix(curr_cts)), group=as.factor(curr_group))
-  # if(length(curr_cts) > 3 & var(curr_cts) > 0){
-  #   if(length(unique(curr_group))>1){
-  #     curr_design <- model.matrix(~as.factor(curr_group))
-  #   }else{
-  #     curr_design <- NULL
-  #   }
-  #   curr_y <- try(estimateDisp(curr_y, curr_design), silent=TRUE)
-  #   if(class(curr_y)[1]!="try-error"){return(curr_y$tagwise.dispersion)}
-  # }
-  return(NA)
-})
-disp_nonzero_batch1 <- disp_nonzero_batch1[!is.na(disp_nonzero_batch1)]
+## Calculate dispersion in each batch
+disp_nonzero_africa <- genDisp_Sep(nonzero_cts=nonzero_cts, batch_sep=batch_sep, group_sep=group_sep, glm_ctrl=glm_ctrl, batch_name="Africa")
+disp_nonzero_G6 <- genDisp_Sep(nonzero_cts=nonzero_cts, batch_sep=batch_sep, group_sep=group_sep, glm_ctrl=glm_ctrl, batch_name="G6")
+disp_nonzero_india <- genDisp_Sep(nonzero_cts=nonzero_cts, batch_sep=batch_sep, group_sep=group_sep, glm_ctrl=glm_ctrl, batch_name="India")
+disp_nonzero_bzl1 <- genDisp_Sep(nonzero_cts=nonzero_cts, batch_sep=batch_sep, group_sep=group_sep, glm_ctrl=glm_ctrl, batch_name="Brazil_1")
+disp_nonzero_bzl2 <- genDisp_Sep(nonzero_cts=nonzero_cts, batch_sep=batch_sep, group_sep=group_sep, glm_ctrl=glm_ctrl, batch_name="Brazil_2")
 
-##  Dispersion in batch 2
-#take batch 2 data
-group_sep_batch2 <- nonzero_cts_batch2 <- list()
-for(j in 1:length(nonzero_cts)){
-  group_sep_batch2[[j]] <- group_sep[[j]][batch_sep[[j]]==2]
-  nonzero_cts_batch2[[j]] <- nonzero_cts[[j]][batch_sep[[j]]==2]
-}
-names(nonzero_cts_batch2) <- names(group_sep_batch2) <- rownames(cts)
-# calculate dispersions
-disp_nonzero_batch2 <- sapply(1:length(nonzero_cts_batch2), function(jj){
-  curr_cts <- nonzero_cts_batch2[[jj]] 
-  curr_group <- group_sep_batch2[[jj]]
-  if(length(curr_cts) > 2 & var(curr_cts) > 0){
-    curr_f <- try(glm.nb(curr_cts ~ as.factor(curr_group), control=glm_ctrl), silent=TRUE)
-    if(class(curr_f)[1]!="try-error"){
-      return(1/summary(curr_f)$theta)
-    }
-  }
-  return(NA)
-})
-disp_nonzero_batch2 <- disp_nonzero_batch2[!is.na(disp_nonzero_batch2)]
 
 
 ####  Compare
-disp_full_lst <- list(Batch1=disp_full_batch1, Batch2=disp_full_batch2)
-disp_nonzero_lst <- list(Batch1=disp_nonzero_batch1, Batch2=disp_nonzero_batch2)
+disp_full_lst <- list(Africa=disp_full_africa, G6=disp_full_G6, India=disp_full_india,
+                      Brazil_1=disp_full_bzl1, Brazil_2=disp_full_bzl2)
+disp_nonzero_lst <- list(Africa=disp_nonzero_africa, G6=disp_nonzero_G6, India=disp_nonzero_india,
+                         Brazil_1=disp_nonzero_bzl1, Brazil_2=disp_nonzero_bzl2)
 
 disp_full_stats <- lapply(c(min, median, mean, max), function(ff){sapply(disp_full_lst, ff)})
 disp_full_stats <- do.call(rbind, disp_full_stats)
@@ -129,4 +108,6 @@ print(round(disp_nonzero_stats, 4))
 
 
 ####  Save result
-save(disp_nonzero_batch1, disp_nonzero_batch2, disp_full_batch1, disp_full_batch2, file="disps_nonzero.RData")
+save(disp_full_africa, disp_full_G6, disp_full_india, disp_full_bzl1, disp_full_bzl2,
+     disp_nonzero_africa, disp_nonzero_G6, disp_nonzero_india, disp_nonzero_bzl1, disp_nonzero_bzl2,
+     file="disps_nonzero.RData")
